8a1104ab0dcc41c0f0f01b92c987752f2e129b45
Tyrone Jensen <56504946+TyroneJensen@users.noreply.github.com>
2024-03-30 13:47:04 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 import datetime
 
 def main():
-  print('Hello world!')
+  print('Hello world! with env variables')
 
   current_time = datetime.datetime.now()
   print(f'Current time: {current_time}')
